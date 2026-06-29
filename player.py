dcaf7248facad6bc2bb5d23c4ad206cf19fd7d09
Gia Lac Ha <hagialac168@gmail.com>
2026-06-29 13:37:49 -0400

    Add Player class with default game-state attributes

---
+++ player.py
@@ -0,0 +1,9 @@
+class Player:
+    def __init__(self, name, hole_cards, chips):
+        self.name = name
+        self.hole_cards = hole_cards
+        self.chips = chips
+        self.current_bet = 0
+        self.total_bet_this_round = 0
+        self.is_folded = False
+        self.is_all_in = False
