3d8049bcb0920c606f1f723e33821b08646e34f1
Gia Lac Ha <hagialac168@gmail.com>
2026-06-29 14:04:30 -0400

    Add hole card dealing logic

--- player.py
+++ player.py
@@ -7,3 +7,13 @@ class Player:
         self.total_bet_this_round = 0
         self.is_folded = False
         self.is_all_in = False
+
+from deck import Deck
+
+my_deck = Deck()
+my_deck.shuffle()
+
+players = [Player("Player1", [], 1000), Player("Player2", [], 1000), Player("Player3", [], 1000)]
+
+for player in players:
+    player.hole_cards = [my_deck.deal(), my_deck.deal()]
