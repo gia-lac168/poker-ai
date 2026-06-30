7b5c40c9d0405d95d796a9fb6f8f8a1c80e7cb07
Gia Lac Ha <hagialac168@gmail.com>
2026-06-30 16:30:58 -0400

    Add complete betting round with fold/call/raise and pot tracking

--- main.py
+++ main.py
@@ -25,15 +25,44 @@ my_deck.deal()
 community_cards.append(my_deck.deal())
 
 pot = 0
+someone_raised = True
+current_bet = 10
+
+while someone_raised:
+    someone_raised = False
+    for player in players:
+        if player.is_folded: #skip player that is already folded
+            continue
+
+        if player.total_bet_this_round == current_bet: #skip player that is already match the bet
+            continue
+
+        print(f"{player.name}'s turn")
+        print("Please select an action:")
+        print("1. Fold")
+        print("2. Call")
+        print("3. Raise")
+        decision = input("Your decision?: ")
+        if decision == "1":
+            print("Fold\n")
+            player.is_folded = True
+
+        elif decision == "2":
+            print("Call\n")
+            player.chips -= current_bet - player.total_bet_this_round
+            pot += current_bet - player.total_bet_this_round
+            player.total_bet_this_round = current_bet
+
+        elif decision == "3":
+            print("Raise")
+            someone_raised = True
+            player.amount_bet = int(input("How much would you like to raise to?: "))
+            print("")
+            current_bet = player.amount_bet
+            player.chips -= current_bet - player.total_bet_this_round
+            pot += current_bet - player.total_bet_this_round
+            player.total_bet_this_round = current_bet
+
 for player in players:
-    print("Please select an action:")
-    print("1. Fold")
-    print("2. Call")
-    decision = input("Your decision?: ")
-    if decision == "1":
-        print("Fold\n")
-        player.is_folded = True
-    else:
-        print("Call\n")
-        player.chips -= 10
-        pot += 10
+    print(f"{player.name}: chips={player.chips}, total_bet={player.total_bet_this_round}, folded={player.is_folded}")
+print(f"\n Total pot: {pot}")
--- player.py
+++ player.py
@@ -3,7 +3,7 @@ class Player:
         self.name = name
         self.hole_cards = hole_cards
         self.chips = chips
-        self.current_bet = 0
+        self.amount_bet = 0
         self.total_bet_this_round = 0
         self.is_folded = False
         self.is_all_in = False
